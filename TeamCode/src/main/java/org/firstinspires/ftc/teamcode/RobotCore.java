1dbbf8239f079237685c9f474be648a45036b745
jordan <jwelty27@jcss.us>
2024-10-14 14:02:27 -0400

    Holy fsther guide this commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCore.java
@@ -22,7 +22,7 @@ public class RobotCore extends OpMode {
     @Override
     public void init(){
 
-        ///commit test
+        // if this works imma punch jordan in the gizards
     }
 
     @Override
